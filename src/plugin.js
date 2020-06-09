379cc6337aab3f492e4df33b4bdf2dd073e17263
Vadim Tiukov <dev.vatson@gmail.com>
2020-06-09 17:37:50 +0300

    fix: extracting components and directives for a component without a template
    - Do nothing if the template is missing;

--- src/plugin.js
+++ src/plugin.js
@@ -3,35 +3,36 @@ const load = require("./load");
 const extract = require("./extract");
 const transform = require("./transform");
 
-const externalScriptTemplate = new Map();
+const extractAndTransform = (code, template = "") => {
+  if (typeof template !== "string" || !template.trim()) {
+    return code;
+  }
 
-const extractAndTransform = (code, template) => {
   const { directives, components } = extract(template);
   return transform(code, components, directives);
-}
+};
 
+const externalScriptTemplate = new Map();
 const filter = createFilter(/.*\.vue/);
 
-module.exports = (options = {}) => {
-  return {
-    async transform(code, id) {
-      if (externalScriptTemplate.has(id)) {
-        return extractAndTransform(code, externalScriptTemplate.get(id));
-      } else if (filter(id)) {
-        const source = await load(id);
+module.exports = () => ({
+  name: "vuetify",
+  async transform(code, id) {
+    if (externalScriptTemplate.has(id)) {
+      return extractAndTransform(code, externalScriptTemplate.get(id));
+    } else if (filter(id)) {
+      const source = await load(id);
 
-        if (source.isExternalScript) {
-          externalScriptTemplate.set(source.scriptPath, source.template);
-          return;
-        } 
-        else if (/\.*vue\?((?!map).)*$/i.test(id) || !source.script) {
-          if (typeof source.script === 'string' && source.script.trim() === '') {
-            code = 'export default {}';
-          }
-
-          return extractAndTransform(code, source.template);
+      if (source.isExternalScript) {
+        externalScriptTemplate.set(source.scriptPath, source.template);
+        return;
+      } else if (/\.*vue\?((?!map).)*$/i.test(id) || !source.script) {
+        if (typeof source.script === "string" && source.script.trim() === "") {
+          code = "export default {}";
         }
+
+        return extractAndTransform(code, source.template);
       }
-    },
-  };
-};
+    }
+  },
+});
